0244756fad1e680945edb9faf5b3a6a8dabd3e80
Andrew Wittrock <andy@wittrock.us>
2020-04-20 00:10:36 -0500

    Add more examples

--- stories/1-Jitter.stories.tsx
+++ stories/1-Jitter.stories.tsx
@@ -1,6 +1,7 @@
 import { Welcome } from '@storybook/react/demo';
 import * as React from 'react';
 import { Jitter } from '../lib/components/Jitter';
+import { Flicker } from '../lib/components/Flicker';
 
 export default {
   title: 'Jitter',
@@ -9,3 +10,5 @@ export default {
 
 export const Default = () => <>This is a <Jitter>Jitter Default</Jitter> example</>;
 export const Multiple = () => <>This is a <Jitter count={2}>Jitter Multiple</Jitter> example</>;
+export const Combined = () => <>This is a combined <Jitter count={3}><Flicker>Flicker Jitter</Flicker></Jitter> example</>;
+export const Multiword = () => <><Jitter>This</Jitter> <Jitter>is</Jitter> <Jitter>a</Jitter> <Jitter>multi-word</Jitter> <Jitter>Jitter</Jitter> <Jitter>example.</Jitter></>;
